23b44625ad01fa73c0bcdfa3b7635872464fa68b
isfong <isfong@qq.com>
2021-01-07 10:37:51 +0800

    up to date

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function createWindow() {
         width: 1400,
         height: 800,
         // icon: __dirname + '/dyh.png',
-        title: '罗森/LAWSON',
+        title: 'OrangeConvenient',
         zoomToPageWidth: true,
     } );
-    win.loadURL( 'https://www.bossdream.com/console/dashboard/data-center/data-repair?brand=zbls' );
+    win.loadURL( 'https://www.bossdream.com/console/dashboard/data-center/data-repair?brand=czbl' );
     win.on( 'closed', () => {
         win = null
     } );
